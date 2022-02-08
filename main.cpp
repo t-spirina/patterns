e4ce10eaa50aa0e38a4a96dabd182a134d543634
[Tanya] <[spirina.tasya@yandex.ru]>
2022-02-08 23:06:00 +0300

    lesson5

--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
-#include <vector>
-#include "fabric_method.h"
-#include "builder.h"
-#include "brige.h"
+#include <iostream>
+#include "facade.h"
+#include "visitor.h"
+#include "observer.h"
+
 int main()
 {
-    //---------------------- fabric_method -------
-    std::cout<<"fabric_method:"<<std::endl;
-    std::vector<Pizza*> order;
-    PizzaFactory* cheesePizzaFactory = new CheesePizzaFactory;
-    PizzaFactory* pepperoniPizzaFactory = new PepperoniPizzaFactory;
-    PizzaFactory* greekPizzaFactory = new GreekPizzaFactory;
-    order.push_back(pepperoniPizzaFactory->createPizza());
-    order.push_back(greekPizzaFactory->createPizza());
-    order.push_back(cheesePizzaFactory->createPizza());
-    for(const auto& pizza: order){
-       pizza->prepare();
-    }
-    std::cout<<std::endl;
 
-    //---------------------- builder-------
-    std::cout<<"builder:"<<std::endl;
-    FirstDaySchedule firstDaySchedule;
-    DayConstructer dayMaker;
-    Day* firstDay = firstDaySchedule.createDaySchedule(dayMaker);
-    firstDay->eventInfo();
-    std::cout<<std::endl;
+  std::cout<<"------ facade -----"<<std::endl;
+  FacadeForMediaPlayer mp;
+  mp.OnAndPlay("1 film");
+  mp.ChangeMovie("2 film");
+  mp.Off();
 
-    //------------------ brige -------------
-    std::cout<<"brige:"<<std::endl;
-    Rectangle* r=new Rectangle((new DrawWithBrush),1.0);
-    r->draw();
-    return 0;
+  std::cout<<"------ observer -----"<<std::endl;
+  Herald* hrld = new Herald;
+  Doctor* doc = new Doctor(*hrld);
+  Child* cld = new Child(*hrld);
+  Blacksmith* blm = new Blacksmith(*hrld);
+  hrld->makeAnnouncement();
+  hrld->setVoicePower(50);
+  hrld->setAnnouncement("first message");
+  hrld->makeAnnouncement();
+
+  std::cout<<"------ visitor -----"<<std::endl;
+  Ingredient* potato = new Ingredient;
+  MenuItem *boild_potato = new MenuItem("boid potato","-",5.5);
+  MenuInfo *newInfo = new MenuInfo;
+  boild_potato->add(potato);
+  potato->SetInfo("potato",5,2.0,0,0.1,0);
+  Ingredient* butter = new Ingredient;
+  butter->SetInfo("butter",1,20.0,10,0.1,10);
+  boild_potato->add(butter);
+  newInfo->visit(boild_potato);
+  //newInfo->visit(potato);
+  return 0;
 }
